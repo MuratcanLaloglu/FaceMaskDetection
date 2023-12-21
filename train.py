5bc1a06559fba6df544fbc55b63d784629744d46
Muratcan Laloğlu <71566447+MuratcanLaloglu@users.noreply.github.com>
2023-12-21 17:48:31 +0300

    Update train.py

--- train.py
+++ train.py
@@ -19,39 +19,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# Constants
 INIT_LR = 1e-4
 EPOCHS = 10  
 BS = 32
 
+# Dataset directory and categories
 DIRECTORY = r"dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 print("[INFO] loading images...")
 
+# Lists to store image data and labels
 data = []
 labels = []
 
+# Loop through each category and load images
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
         img_path = os.path.join(path, img)
+
+        # Load image, convert to array, and preprocess
         image = load_img(img_path, target_size=(224, 224))
         image = img_to_array(image)
         image = preprocess_input(image)
 
+        # Append image and label to lists
         data.append(image)
         labels.append(category)
 
+# Convert labels to binary format and one-hot encode
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
+# Convert data and labels to numpy arrays
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
+# Split the data into training and testing sets
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
                                                   test_size=0.20, stratify=labels, random_state=42)
 
+# Data augmentation
 aug = ImageDataGenerator(
     rotation_range=20,
     zoom_range=0.15,
@@ -61,9 +72,11 @@ aug = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode="nearest")
 
+# Base model using MobileNetV2 with pre-trained weights
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
                         input_tensor=Input(shape=(224, 224, 3)))
 
+# Define the model 
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -71,19 +84,20 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
+# Combine the base model and custom head
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-
+# Freeze the layers of the base model
 for layer in baseModel.layers:
     layer.trainable = False
 
-
+# Compile the model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
 
-
+# Train the model
 print("[INFO] training head...")
 H = model.fit(
     aug.flow(trainX, trainY, batch_size=BS),
@@ -92,15 +106,17 @@ H = model.fit(
     validation_steps=len(testX) // BS,
     epochs=EPOCHS)
 
+# Evaluate the model on the test set
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-
+# Convert predictions to class labels
 predIdxs = np.argmax(predIdxs, axis=1)
 
-
+# Print classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
                             target_names=lb.classes_))
 
+# Save the trained model
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model")
